4d1118b321906b7a3e33ae0fc0f56738c083ddb7
Monika-Gebre <monikagebre2004@gmail.com>
2025-01-02 17:04:47 +0000

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,22 @@
+// SMALLER SCREEN HEADER
+function toggleSmallerMenu(){
+  document.getElementById("smaller-nav-menu").classList.toggle("active");
+}
+
+
+// POP PROJECT PREVIEW
+function togglePopup(){
+  document.getElementById("project-popup").classList.toggle("active");
+}
+
+function togglePopupTwo(){
+  document.getElementById("project-popup-two").classList.toggle("active");
+}
+
+function togglePopupThree(){
+  document.getElementById("project-popup-three").classList.toggle("active");
+}
+
+function togglePopupFour(){
+  document.getElementById("project-popup-four").classList.toggle("active");
+}
